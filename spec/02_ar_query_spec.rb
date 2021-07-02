5a071b2ea9cec22f8632f59ffa310a72bf2cb554
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-07-02 12:58:35 -0700

    update specs

--- spec/02_ar_query_spec.rb
+++ spec/02_ar_query_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "ActiveRecord queries" do
     it "updates the fan object created in the previous test to now be a fan of Led Zeppelin. (Again, check the artists table to find the correct foreign key)." do
       fan = Fan.where(name: NAME, artist_id: 50).last
       change_yourself_to_be_a_fan_of_led_zeppelin
-      expect(fan).to exist, "Fan was not found. Make sure that you've updated the NAME constant to match the name you're giving the new Fan."
+      expect(fan).not_to be_nil, "Fan was not found. Make sure that you've updated the NAME constant to match the name you're giving the new Fan."
       fan.reload
       expect(fan.artist_id).to eq(22)
     end
@@ -34,7 +34,7 @@ RSpec.describe "ActiveRecord queries" do
   describe "remove_yourself_as_a_fan" do 
     it "removes the fan object updated in the previous test"  do 
       fan = Fan.where(name: NAME, artist_id: 22).last
-      expect(fan).to exist, "No fan was found. Make sure that you've updated the NAME constant to match the name used in your method calls."
+      expect(fan).not_to be_nil, "No fan was found. Make sure that you've updated the NAME constant to match the name used in your method calls."
       remove_yourself_as_a_fan
       expect(Fan.all).not_to include(fan)
     end
